6afc78d35d7f5bc48b855ad3d072e995e376127d
Zion0707 <346445684@qq.com>
2020-12-09 16:22:53 +0800

    add config base file

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,41 +1,8 @@
 // 开发配置
 const { resolve, join } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const baseConfig = require('./webpack.config.base.js');
 
 module.exports = {
-    entry: resolve(__dirname, '../src/index.js'),
-
-    output: {
-        path: resolve(__dirname, '../dist'),
-        filename: '[name].js?=' + Date.parse(new Date()),
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpeg|jpg|gif)$/,
-                loader: 'url-loader',
-                options: {
-                    // 8kb 以下采用base64方式
-                    limit: 8 * 1024,
-                },
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.(less|css)$/,
-                use: ['style-loader', 'css-loader', 'less-loader'],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-        ],
-    },
-
     devServer: {
         hot: true,
         proxy: {
@@ -46,24 +13,10 @@ module.exports = {
         port: 8086,
     },
 
-    plugins: [
-        // 打包html模板
-        new HtmlWebpackPlugin({
-            template: resolve(__dirname, '../src/index.html'),
-            favicon: resolve(__dirname, '../src/statics/imgs/favicon.ico'),
-        }),
-    ],
-
-    resolve: {
-        // 省略编写文件后缀名
-        extensions: ['.js', '.jsx', '.css', '.less'],
-        // 使用别名，简化引用路径
-        alias: {
-            _less: resolve(__dirname, '../src/less'),
-        },
-    },
-
     mode: 'development',
+
+    // 引用公共部分
+    ...baseConfig,
 };
 
 console.log('--------------测试开发环境----------------');
--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import '_less/index';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '_pages/home/index';
 
-ReactDOM.render(<div>React</div>, document.getElementById('root'));
+ReactDOM.render(
+    <div className="main">
+        <Router>
+            <Route path="/" exact component={Home} />
+        </Router>
+    </div>,
+    document.getElementById('root')
+);
