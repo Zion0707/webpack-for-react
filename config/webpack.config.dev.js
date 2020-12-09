4d6595015d9661081893e6b1b64fe704745c6a23
Zion0707 <346445684@qq.com>
2020-12-09 11:11:54 +0800

    初始化

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,32 @@
+// 开发配置
+const { resolve, join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: resolve(__dirname, '../src/index.js'),
+    output: {
+        path: resolve(__dirname, '../dist'),
+        filename: '[name].js?=' + Date.parse(new Date()),
+        publicPath: './', //或cdn地址 https://cdn.example.com/
+    },
+    module: {
+        rules: [],
+    },
+    devServer: {
+        hot: true,
+        proxy: {
+            '/api': 'http://localhost:7001',
+        },
+        contentBase: join(__dirname, 'dist'),
+        host: '0.0.0.0',
+        port: 8086,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, '../src/index.html'),
+        }),
+    ],
+    mode: 'development',
+};
+
+console.log('--------------测试开发环境----------------');
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,23 @@
+// 生产配置
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: resolve(__dirname, '../src/index.js'),
+    output: {
+        path: resolve(__dirname, '../dist'),
+        filename: '[name][chunkhash:8].js',
+        publicPath: './', //或cdn地址 https://cdn.example.com/
+    },
+    module: {
+        rules: [],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: resolve(__dirname, '../src/index.html'),
+        }),
+    ],
+    mode: 'production',
+};
+
+console.log('--------------生产打包环境----------------');
