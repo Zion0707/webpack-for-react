6002d9516a7c60da8aeed21714b07cac6527f8e6
Zion0707 <346445684@qq.com>
2020-12-14 13:42:28 +0800

    修复不支持es6 ...扩展字符

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,11 @@ import photos from '_statics/imgs/02.jpg';
 import '_less/home/index';
 
 const Home = () => {
-    useEffect(() => {}, []);
+    useEffect(() => {
+        const obj1 = { name: 'webpack' };
+        const obj2 = { version: '4.x', ...obj1 };
+        console.log(obj2);
+    }, []);
     return (
         <div className="home">
             <h4>home</h4>
